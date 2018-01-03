ed5db01473122633c211d183b11bdbb693baeb67
Britni <bcanale@stuy.edu>
2018-01-03 14:44:16 -0500

    Adding first few files, .gitignore and SudokuWindow

---
+++ SudokuWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SudokuWindow extends JFrame implements ActionListener{
+    public SudokuWindow(){}
+    public void actionPerformed(ActionEvent e){}
+    public static void main(String[] args){}
+}

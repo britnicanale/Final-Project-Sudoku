c6ac37d11635be21e1f79d0003e204eb687e9f03
jrubin13 <jrubin@stuy.edu>
2018-01-03 14:43:15 -0500

    created Sudoku fiile

---
+++ Sudoku.java
@@ -0,0 +1,15 @@
+public class Sudoku() {
+    private int[][] data;
+    private int[][] input;
+    private Random randgen;
+    public Sudoku() {}
+    public Sudoku(int seed) {}
+    private boolean checkRows(){}
+    private boolean checkColumns(){}
+    private boolean checkBoxes(){}
+    public String displaySolution(){}
+    public boolean checkAnswer(){}
+    public boolean checkSolution(){}
+    private void createPuzzle(){}
+    public void changeInput(int row, int col, int value){}
+}

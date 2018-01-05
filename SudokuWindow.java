bd80ad2d4fb34682c0dc40c3a50bbc1ff38d460f
Britni <bcanale@stuy.edu>
2018-01-05 14:49:50 -0500

    Made buttons, added connection to Sudoku

--- SudokuWindow.java
+++ SudokuWindow.java
@@ -3,7 +3,33 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class SudokuWindow extends JFrame implements ActionListener{
-    public SudokuWindow(){}
+
+    private Container pane;
+
+    public SudokuWindow(){
+
+	Sudoku puzzle = new Sudoku();
+
+	this.setTitle("Sudoku");
+        this.setSize(300,300);
+        this.setLocation(0,0);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        pane = this.getContentPane();
+
+	pane = this.getContentPane();
+
+	pane.setLayout(new GridLayout(9, 9));                      //Britni -- Creates a 9 x 9 Grid for the board
+
+	for(int i = 1; i < 82; i++){                               //Britni -- Creates 81 JTextBoxes that fit within the board
+	    JTextField b = new JTextField(puzzle.getData(i / 9, i % 9));
+	    b.addActionListener(this);
+	    pane.add(b);
+	}
+    }
     public void actionPerformed(ActionEvent e){}
-    public static void main(String[] args){}
+    public static void main(String[] args){
+	SudokuWindow s = new SudokuWindow();
+	s.setVisible(true);
+    }
 }

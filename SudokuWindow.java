94e179f5c5f774741a175dd76caabf9c58804094
Britni <bcanale@stuy.edu>
2018-01-07 17:39:25 -0500

    Creating GUI, making two different panes in the GUI, ALSO adding Jeremy's edits to Sudoku because there are merge issues that we are currently fixing and I need to test

--- SudokuWindow.java
+++ SudokuWindow.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 
 public class SudokuWindow extends JFrame implements ActionListener{
 
-    private Container pane;
+    //private Container pane;
 
     public SudokuWindow(){
 
@@ -15,16 +15,28 @@ public class SudokuWindow extends JFrame implements ActionListener{
         this.setLocation(0,0);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        pane = this.getContentPane();
+	JPanel pane = new JPanel(new BorderLayout());
 
-	pane = this.getContentPane();
+	JPanel chessBoardPanel = new JPanel(new GridLayout(9, 9));
 
-	pane.setLayout(new GridLayout(9, 9));                      //Britni -- Creates a 9 x 9 Grid for the board
+	JPanel buttonPane = new JPanel();
+
+	pane.add(sudokuPane, BorderLayout.CENTER);
+
+	pane.add(buttonPane, BorderLayout.PAGE_END);
+
+	this.getContentPane().add(pane);
+
+
+	//pane = this.getContentPane();
+
+
+	//pane.setLayout(new GridLayout(9, 9));                      //Britni -- Creates a 9 x 9 Grid for the board
 
 	for(int i = 1; i < 82; i++){                               //Britni -- Creates 81 JTextBoxes that fit within the board
 	    JTextField b = new JTextField(puzzle.getData(i / 9, i % 9));
 	    b.addActionListener(this);
-	    pane.add(b);
+	    sudokuPane.add(b);
 	}
     }
     public void actionPerformed(ActionEvent e){}

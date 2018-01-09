ea95d3fdabddb6f82281745418604b39c9d626d7
Britni <bcanale@stuy.edu>
2018-01-08 20:53:20 -0500

    Added additional button to new pane, successful. Fixed error, realized I can't add ints to JTextField, converted them to ints, now SudokuWindow() is able to interact with Sudoku()

--- SudokuWindow.java
+++ SudokuWindow.java
@@ -6,13 +6,13 @@ public class SudokuWindow extends JFrame implements ActionListener{
 
     //private Container pane;
     private Sudoku puzzle;
-
+    //private JTextField b;
     public SudokuWindow(){
 
 	puzzle = new Sudoku();
 
 	this.setTitle("Sudoku");
-        this.setSize(300,300);
+        this.setSize(300,400);
         this.setLocation(0,0);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -30,19 +30,28 @@ public class SudokuWindow extends JFrame implements ActionListener{
 
 	this.getContentPane().add(pane);
 
+	JButton createPuzzle = new JButton("Create Puzzle");
+	
+	createPuzzle.addActionListener(this);
 
-	//pane = this.getContentPane();
+	buttonPane.add(createPuzzle);
 
 
-	//pane.setLayout(new GridLayout(9, 9));                      //Britni -- Creates a 9 x 9 Grid for the board
-
-	for(int i = 0; i < 81; i++){                               //Britni -- Creates 81 JTextBoxes that fit within the board
-	    JTextField b = new JTextField(puzzle.getData(i / 9, i % 9));
-	    b.addActionListener(this);
-	    sudokuPane.add(b);
+	for(int i = 0; i < 9; i++){                               //Britni -- Creates 81 JTextBoxes that fit within the board
+	    for(int j = 0; j < 9; j++){
+		
+		JTextField b = new JTextField("" + puzzle.getData(i, j));
+		b.addActionListener(this);
+		sudokuPane.add(b);
+	    }
 	}
     }
-    public void actionPerformed(ActionEvent e){}
+    public void actionPerformed(ActionEvent e){
+	String s = e.getActionCommand();
+	if(s.equals("Create Puzzle")){
+	    //b.setText("0");
+	}
+    }
     public static void main(String[] args){
 	SudokuWindow s = new SudokuWindow();
 	s.setVisible(true);

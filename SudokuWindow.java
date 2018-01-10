25d9957e148d09e309f3120508ec94b084a4b6a8
Britni <bcanale@stuy.edu>
2018-01-10 14:50:22 -0500

    Experimented with background

--- SudokuWindow.java
+++ SudokuWindow.java
@@ -30,6 +30,7 @@ public class SudokuWindow extends JFrame implements ActionListener{
 	//NOTE: THE NEXT SIX LINES ARE EDITED BUT TAKEN FROM THE FOLLOWING LINK: https://stackoverflow.com/questions/2554684/multiple-layout-managers-in-java  I used this because I am having a problem with panes that I cannot solve and am using this for testing purposes at the moment
 
 	pane = new JPanel(new BorderLayout());
+	pane.setBackground(Color.WHITE);
 
 	sudokuPane = new JPanel(new GridLayout(9, 9));
 
